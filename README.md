4a8c1a030e9c95cd1c595594c3ae4d2f93dedd9d
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2018-08-21 18:14:24 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # redmine_crm_templates_grabber
+
+## Description
+
+This tool solves one task - downloads a bunch of Redmine HelpDesk templates in a format, which allows to import them into Confluence without issues of cyrillic texts.
+
+How does it work:
+- login to Redmine using Selenium,
+- save each page using Selenium,
+- transform each page to Docx format using Pandoc. 
+
+## Dependencies 
+
+<pre>
+sudo pip3 install selenium
+sudo apt-get install pandoc firefox
+</pre>
+
+Download geckodriver from https://github.com/mozilla/geckodriver/releases and unpack to repo root. 
+
+## Execution
+
+<pre>
+PATH="./:$PATH" python3 grabber.py --login LOGIN --password PASSWORD --project PROJECT --host HOST --start_id START_ID --end_id END_ID
+</pre>
---
+++ grabber.py
@@ -0,0 +1,61 @@
+# Dependencies - pandoc, selenium, firefox
+from selenium import webdriver
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException        
+from os import system
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--login', required=True)
+parser.add_argument('--password', required=True)
+parser.add_argument('--project', required=True)
+parser.add_argument('--host', required=True)
+parser.add_argument('--start_id', required=True, type=int)
+parser.add_argument('--end_id', required=True, type=int)
+
+results = parser.parse_args()
+
+
+login=results.login
+password=results.password
+project=results.project
+host=results.host
+start=results.start_id
+end=results.end_id
+
+url_template='https://{}/projects/{}'.format(host, project) + '/crm_templates/{}/edit'
+login_url = 'https://{}/login'.format(host)
+
+# authorize
+driver = webdriver.Firefox()
+print(login_url)
+driver.get(login_url)
+driver.find_element_by_id('username').send_keys(login)
+driver.find_element_by_id('password').send_keys(password)
+driver.find_element_by_id('autologin').click()
+driver.find_element_by_id('login-submit').click()
+# save each url
+
+for i in range(start,end+1):
+    url = url_template.format(i)
+    print(url)
+    sleep(3)    
+    driver.get(url)
+    # take title and content
+    # if error then it is probably deleted page
+    try:
+        title = driver.find_element_by_id('crm_template_title').get_attribute('value')
+        content = driver.find_element_by_id('crm_template_content').text
+    except NoSuchElementException:
+        continue
+    # save to file
+    print(title)
+    print(content)
+    # Hacks for importing cyrillic CRM templates to Confluence 
+    content = "<br>".join(content.split("\n"))
+    f = open(title+".html","wb")
+    f.write(content.encode('utf-8'))
+    f.close()
+    system("pandoc -s \"{0}.html\" -o \"{0}.docx\"".format(title))
+driver.close()
